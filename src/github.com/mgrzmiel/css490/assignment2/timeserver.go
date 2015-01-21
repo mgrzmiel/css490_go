7e84ad144ab71799209345d2b7b2d6bcc5dc6ddd
Magdalena Grzmiel <grzmiel@outlook.com>
2015-01-20 21:21:09 -0800

    Add mutex around session storage to solve concurrency issues, add escaping of user name in order to prevent XSS.

--- src/github.com/mgrzmiel/css490/assignment2/timeserver.go
+++ src/github.com/mgrzmiel/css490/assignment2/timeserver.go
@@ -2,7 +2,7 @@
 // Magdalena Grzmiel
 // Assignments #2
 // Copyright 2015 Magdalena Grzmiel
-// This is a simple example of http server.
+// This program is an example of personlized http server.
 
 package main
 
@@ -10,26 +10,59 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"html"
 	"log"
 	"net/http"
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
+// declare the RWMutex structure
+var sessionsSyncLoc *sync.RWMutex
+
+// declare the map for uuid and user's names
 var sessions map[string]string
 
-// logIn is a function which read the user name from request
+// logIn function
+// If the name is provided in the request, the login function generate
+// uuid and add it together with name to map. Then redirect page to index.html
+//endpoint.
+// If the name is an empty string, it just display simple message
 func logIn(res http.ResponseWriter, req *http.Request) {
-	name := req.URL.Query().Get("name")
-	id := generateUniqueId()
-	sessions[id] = name
-	cookie := http.Cookie{Name: "uuid", Value: id, Path: "/"}
-	http.SetCookie(res, &cookie)
-	http.Redirect(res, req, "/index.html", http.StatusFound)
+	fmt.Println(req)
+	//retrive the name form URL
+	name := req.FormValue("name")
+	name = html.EscapeString(name)
+	if name != "" {
+		id := generateUniqueId()                                  // generate uuid
+		sessionsSyncLoc.Lock()                                    // before modifying the map, lock it
+		sessions[id] = name                                       // add name with uuid to map
+		sessionsSyncLoc.Unlock()                                  // unlock map
+		cookie := http.Cookie{Name: "uuid", Value: id, Path: "/"} // create cookie
+		http.SetCookie(res, &cookie)                              //set the cookie and redirect to the /index.htm endpoint
+		http.Redirect(res, req, "/index.html", http.StatusFound)
+	} else {
+		// if the provided input - name is empty, display this message
+		res.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(
+			res,
+			`<html>
+			<body>
+			<form action="login">
+			  C'mon, I need a name.
+			</form>
+			</p>
+			</body>
+			</html>`,
+		)
+	}
 }
 
+// generateUniqueId
+// This function generates cookie containing a univerally unique identifier
 func generateUniqueId() string {
 	cmd := exec.Command("/usr/bin/uuidgen")
 	var out bytes.Buffer
@@ -44,8 +77,12 @@ func generateUniqueId() string {
 }
 
 // loginForm function
+// If user is not login, it displays login form
+// Otherwise display the greeting message
 func loginForm(res http.ResponseWriter, req *http.Request) {
-	name, correctlyLogIn:= getNameAndCookie(res, req)
+	fmt.Println(req)
+	//check if the user is login -
+	name, correctlyLogIn := getNameAndCookie(res, req)
 	res.Header().Set("Content-Type", "text/html")
 	if !correctlyLogIn {
 		fmt.Fprintf(
@@ -73,13 +110,20 @@ func loginForm(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// invalidate cookie
+// It invalidates cookies since no name exists for that cookie in map
 func invalidateCookie(res http.ResponseWriter) {
+	// set the experiation date to yesterday
 	expire := time.Now().AddDate(-1, 0, -1)
 	cookie2 := http.Cookie{Name: "uuid", Path: "/", Expires: expire}
 	http.SetCookie(res, &cookie2)
 }
 
+//logout
+// It invalidate the cookie since user is no longer liginh
+// and displat good bye message
 func logOut(res http.ResponseWriter, req *http.Request) {
+	fmt.Println(req)
 	invalidateCookie(res)
 	fmt.Fprintf(
 		res,
@@ -90,35 +134,45 @@ func logOut(res http.ResponseWriter, req *http.Request) {
 		<p>Good-bye.</p>
 		</body>
 		</html>`,
-		)
+	)
 }
 
-func getNameAndCookie (res http.ResponseWriter, req *http.Request) (string, bool){
+//getNameAndCookie
+//check if the cookie is set up and if the name for that cookie exists in map
+// based on that, it sets up the correctlyLogIn variable.
+func getNameAndCookie(res http.ResponseWriter, req *http.Request) (string, bool) {
 	var name string
 	var ok bool
 	var cookie, err = req.Cookie("uuid")
+	//correctlyLogIn - means that both cookie and name exists
 	correctlyLogIn := false
 	// if the cookie is set up
 	if err == nil {
-		// retrive the name
+		// retrive the name, before the access to map, lock it
+		sessionsSyncLoc.RLock()
 		name, ok = sessions[cookie.Value]
-		// if the name exist, print greetings
+		sessionsSyncLoc.RUnlock()
+		// if the name exist, set correctllyLogIn to true
 		if ok {
 			correctlyLogIn = true
-		// no name so invalidate cookie
+			// no name so invalidate cookie
 		} else {
 			invalidateCookie(res)
 		}
 	}
+
 	return name, correctlyLogIn
 }
 
-// getTime is a function which display the time on the webside
+// getTime
+// It is caleed when the /time endpoint is used
+// It displayes the time on the webside
 func getTime(res http.ResponseWriter, req *http.Request) {
+	fmt.Println(req)
 	now := time.Now().Format("3:04:05 PM")
 	name, correctlyLogIn := getNameAndCookie(res, req)
 	res.Header().Set("Content-Type", "text/html")
-	if (correctlyLogIn){
+	if correctlyLogIn {
 		fmt.Fprintf(
 			res,
 			`<doctype html>
@@ -130,13 +184,13 @@ func getTime(res http.ResponseWriter, req *http.Request) {
 			</style>
 			</head>
 			<body>
-			<p>The time is now <span class="time">`+now+`</span>, `+name+ 
-			`.</p>
+			<p>The time is now <span class="time">`+now+`</span>, `+name+
+				`.</p>
 			</body>
 			</html>`,
 		)
-	}else{
-	fmt.Fprintf(
+	} else {
+		fmt.Fprintf(
 			res,
 			`<doctype html>
 	        <html>
@@ -154,9 +208,13 @@ func getTime(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-// unknownRoute is a function which display "These are not the URLs you're looking for"
+// unknownRoute
+// If the endpint is not known, this method is called
+// It displays following message:
+// "These are not the URLs you're looking for"
 // It also sets the status code to 404
 func unknownRoute(res http.ResponseWriter, req *http.Request) {
+	fmt.Println(req)
 	res.WriteHeader(http.StatusNotFound)
 	res.Header().Set("Content-Type", "html")
 	fmt.Fprintf(
@@ -176,6 +234,7 @@ func main() {
 	var port int
 	var version bool
 	sessions = make(map[string]string)
+	sessionsSyncLoc = new(sync.RWMutex)
 
 	// parse the flags
 	flag.IntVar(&port, "port", 8080, "used port")
@@ -185,7 +244,7 @@ func main() {
 	// if user type -V, the V flag is set up to true
 	if version {
 		// display the information about the version
-		fmt.Println("version 1.4")
+		fmt.Println("version 1.9")
 		// otherwise run the server
 	} else {
 		portNr := strconv.Itoa(port)
